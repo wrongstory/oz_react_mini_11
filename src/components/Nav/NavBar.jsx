75f800c42c7b3c03e7b05d6cf33433ffed673b5c
L <lmwoo0327@naver.com>
2025-06-19 16:47:32 +0900

    feat. NavBar에 입력창 추가 및 입력값 상태 관리

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
+  const [searchMovie, setSearchMovie] = useState("");
+
+  const handleChange = (e) => {
+    setSearchMovie(e.target.value);
+  };
+
   return (
     <nav className="bg-gray-800 text-white px-6 py-4 flex justify-between items-center shadow-md">
       <Link to="/" className="text-xl font-bold hover:text-yellow-400">
         🎬 L's Movie
       </Link>
+      <input
+        type="text"
+        placeholder="영화 이름 입력.."
+        value={searchMovie}
+        onChange={handleChange}
+        className="text-black"
+      />
     </nav>
   );
 }
