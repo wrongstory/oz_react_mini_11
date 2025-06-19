99eedd771ede9ebcefddee2e611cbf3166ce1870
L <lmwoo0327@naver.com>
2025-06-19 11:40:23 +0900

    feat. fetch TMDB API로 변경 후 한글 패치

--- src/api/movie.js
+++ src/api/movie.js
@@ -1,11 +1,28 @@
+const API_URL = "https://api.themoviedb.org/3";
+const ACCESS_TOKEN = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+  },
+};
+
 export async function getMovieList() {
-  const res = await fetch("/asset/dumdata/movieListData.json");
+  const res = await fetch(`${API_URL}/movie/popular?language=ko-KR`, options);
+  if (!res.ok) {
+    throw new Error("API 호출 실패!!!!");
+  }
   const data = await res.json();
   return data.results;
 }
 
 export async function getMovieDetailById(id) {
-  const res = await fetch("/asset/dumdata/movieDetailData.json");
+  const res = await fetch(`${API_URL}/movie/${id}?language=ko-KR`, options);
+  if (!res.ok) {
+    throw new Error("API 호출 실패!!!!");
+  }
   const data = await res.json();
-  return data.results.find((movie) => movie.id === Number(id));
+  return data;
 }
