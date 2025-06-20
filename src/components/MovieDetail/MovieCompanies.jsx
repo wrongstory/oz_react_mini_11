0a9febfa9404a2970690ae72ce635b5c55a46ee3
L <lmwoo0327@naver.com>
2025-06-20 11:50:18 +0900

    feat. Detailnpaged에 모드 테마 적용

--- src/components/MovieDetail/MovieCompanies.jsx
+++ src/components/MovieDetail/MovieCompanies.jsx
@@ -3,23 +3,25 @@ export default function MovieCompanies({ companies, imageBase }) {
 
   return (
     <div className="mt-8">
-      <h3 className="text-2xl font-bold text-black mb-4 border-b border-gray-600 pb-2">
+      <h3 className="text-2xl font-bold text-black dark:text-white mb-4 border-b border-gray-600 pb-2">
         제작사
       </h3>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
         {companies.map((company) => (
           <div
             key={company.id}
-            className="flex flex-col items-center justify-center bg-gray-800 px-5 py-4 rounded-lg shadow gap-2 h-full"
+            className="flex flex-col items-center justify-center 
+              bg-gray-100 dark:bg-gray-800 
+              px-5 py-4 rounded-lg shadow gap-2 h-full"
           >
             {company.logo_path && (
               <img
                 src={`${imageBase}${company.logo_path}`}
                 alt={company.name}
-                className="h-12 object-contain rounded bg-white p-1"
+                className="h-12 object-contain rounded bg-white dark:bg-gray-200 p-1"
               />
             )}
-            <span className="text-sm text-white font-medium text-center">
+            <span className="text-sm text-black dark:text-white font-medium text-center">
               {company.name}
             </span>
           </div>
